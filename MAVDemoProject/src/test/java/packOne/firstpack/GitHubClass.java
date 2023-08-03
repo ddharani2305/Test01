d9b83e55b7ada2441ceae9cbfdf705e742d95965
Srinivasa Rao <ddharani2305@gmail.com>
2023-08-04 00:35:08 +0530

    added test folder

---
+++ MAVDemoProject/src/test/java/packOne/firstpack/GitHubClass.java
@@ -0,0 +1,14 @@
+package packOne.firstpack;
+
+public class GitHubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("success");
+System.out.println("modification");
+System.out.println("vhbfsjkd");
+System.out.println("dfghj");
+	}
+	
+
+}
